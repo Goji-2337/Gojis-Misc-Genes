6aa20e6f5dde83b06458bce7a54306c6884d5ebc
Taranchuk <nfhfyxer@gmail.com>
2025-03-29 23:47:14 +0200

    project setup

---
+++ 1.5/Source/GojisMiscGenesMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace GojisMiscGenes
+{
+    public class GojisMiscGenesMod : Mod
+    {
+        public GojisMiscGenesMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("GojisMiscGenesMod").PatchAll();
+        }
+    }
+}
